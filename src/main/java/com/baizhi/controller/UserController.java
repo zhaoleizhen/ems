a33a32fc4f935808bea127f76584da5d06d1f799
hrb <dakubo@163.com>
2019-04-23 10:27:52 +0800

    user实体类和

--- src/main/java/com/baizhi/controller/UserController.java
+++ src/main/java/com/baizhi/controller/UserController.java
@@ -5,7 +5,6 @@ import com.baizhi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("user")
